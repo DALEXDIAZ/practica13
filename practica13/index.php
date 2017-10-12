80a8e65fdea61444eaa92314a8fcb3e669111cab
DANIEL ALEJANDRO NUCAMENDI DIAZ <30698322+DALEXDIAZ@users.noreply.github.com>
2017-10-11 21:00:08 -0500

    Add files via upload

---
+++ practica13/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="procesar.php" method="POST" name="datosPersonales" >
+  Nombre:<br>
+  <input type="text" name="nombreAlumno"  value="" placeholder="Introduce tu nombre">
+  <br>
+  Apellidos:<br>
+  <input type="text" name="apellidosAlumno" value="" placeholder="Introduce tus apellidos">
+  <br>
+  Edad:<br>
+  <input type="text" name="edad" value="" placeholder="Introduce tu edad">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+
+<p>DANIEL ALEJANDRO NUCAMENDI DIAZ</p>
+
+</body>
+</html>
---
+++ practica13/procesar.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<?php
+$nombre = $_POST['nombreAlumno'];
+$apellidos = $_POST['apellidosAlumno'];
+$edad = $_POST["edad"];
+echo 'TU NOMBRE ES: '.$nombre.' '.$apellidos.'<br>';
+echo 'tu edad es: '.$edad.' años';
+?>
